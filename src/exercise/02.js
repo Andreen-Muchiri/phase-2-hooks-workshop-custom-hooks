34ecf3d593b97505b932d1ccd183dab7158c6e7e
Andreen-Muchiri <andreenshii@gmail.com>
2022-10-02 23:20:58 +0300

    Complete assignment

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -2,56 +2,103 @@ import styled from "styled-components";
 import React, { useEffect, useState } from "react";
 
 /* ✅ modify this usePokemon custom hook to take in a query as an argument */
-export function usePokemon() {
-  /* ✅ this hook should only return one thing: an object with the pokemon data */
-}
 
-function Pokemon({ query }) {
+  /* ✅ this hook should only return one thing: an object with the pokemon data */
+ 
+  export function usePokemon(query) {
+    const [{ data, errors, status }, setState] = useState({
+      data: null,
+      errors: null,
+      status: "idle",
+    });
+
+    useEffect(() => {
+      setState(state => ({ ...state, errors: null, status: "pending" }));
+      fetch(`https://pokeapi.co/api/v2/pokemon/${query}`)
+        .then(r => {
+          if (r.ok) {
+            return r.json();
+          } else {
+            return r.text().then(err => {
+              throw err;
+            });
+          }
+        })
+        .then(data => {
+          setState({ data, errors: null, status: "fulfilled" });
+        })
+        .catch(err => {
+          setState({ data: null, errors: [err], status: "rejected" });
+        });
+    }, [query]);
+
+    return { data, status, errors };
+  }
+
+
   /* 
    ✅ move the code from the useState and useEffect hooks into the usePokemon hook
    then, call the usePokemon hook to access the pokemon data in this component
   */
-  const [pokemon, setPokemon] = useState(null);
-  useEffect(() => {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${query}`)
-      .then(r => r.json())
-      .then(setPokemon);
-  }, [query]);
+  // const [pokemon, setPokemon] = useState(null);
+  // useEffect(() => {
+  //   fetch(`https://pokeapi.co/api/v2/pokemon/${query}`)
+  //     .then(r => r.json())
+  //     .then(setPokemon);
+  // }, [query]);
 
   // 🚫 don't worry about the code below here, you shouldn't have to touch it
-  if (!pokemon) return <h3>Loading...</h3>;
-
-  return (
-    <div>
-      <h3>{pokemon.name}</h3>
-      <img
-        src={pokemon.sprites.front_default}
-        alt={pokemon.name + " front sprite"}
-      />
-    </div>
-  );
-}
-
-export default function App() {
-  const [query, setQuery] = useState("charmander");
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    setQuery(e.target.search.value);
+ 
+  function Pokemon({ query }) {
+    const { data: pokemon, status, errors } = usePokemon(query);
+  
+    if (status === "idle" || status === "pending") {
+      return <h3>Loading....</h3>;
+    }
+  
+    if (status === "rejected") {
+      return (
+        <div>
+          <h3>Error</h3>
+          {errors.map(e => (
+            <p key={e}>{e}</p>
+          ))}
+        </div>
+      );
+    }
+  
+    return (
+      <div>
+        <h3>{pokemon.name}</h3>
+        <img
+          src={pokemon.sprites.front_default}
+          alt={pokemon.name + " front sprite"}
+        />
+      </div>
+    );
   }
-
-  return (
-    <Wrapper>
-      <h1>PokéSearcher</h1>
-      <Pokemon query={query} />
-      <form onSubmit={handleSubmit}>
-        <input type="text" name="search" defaultValue={query} />
-        <button type="submit">Search</button>
-      </form>
-    </Wrapper>
-  );
-}
-
+  
+  export default function App() {
+    const [query, setQuery] = useState("charmander");
+  
+    function handleSubmit(e) {
+      e.preventDefault();
+      setQuery(e.target.search.value);
+    }
+  
+    return (
+      <Wrapper>
+        <h1>PokéSearcher</h1>
+        <Pokemon query={query} />
+        <form onSubmit={handleSubmit}>
+          <input type="text" name="search" defaultValue={query} />
+          <button type="submit">Search</button>
+        </form>
+      </Wrapper>
+    );
+  }
+  
+ 
 const Wrapper = styled.section`
   box-shadow: 4px 4px 8px rgba(0, 0, 0, 0.15);
   display: grid;
@@ -77,3 +124,4 @@ const Wrapper = styled.section`
     width: 100%;
   }
 `;
+
